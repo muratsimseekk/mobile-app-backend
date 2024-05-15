3b37d71aeb5dac0ce6b9d46b3ca3cf2b9602a495
Murat Simsek <muratsimseekk@gmail.com>
2024-05-15 17:38:10 +0300

    Spring project has been created .

---
+++ src/main/java/com/kitapmobile/Spring/Project/For/Mobile/Book/App/SpringProjectForMobileBookAppApplication.java
@@ -0,0 +1,13 @@
+package com.kitapmobile.Spring.Project.For.Mobile.Book.App;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringProjectForMobileBookAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringProjectForMobileBookAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kitapmobile/Spring/Project/For/Mobile/Book/App/SpringProjectForMobileBookAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kitapmobile.Spring.Project.For.Mobile.Book.App;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringProjectForMobileBookAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
