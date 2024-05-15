326c533e1f06d1a906a75283a108ceefd4f1d756
Murat Simsek <muratsimseekk@gmail.com>
2024-05-15 19:07:42 +0300

    Add fields to Category entity

--- src/main/java/com/kitapmobile/Spring/Project/For/Mobile/Book/App/entity/Category.java
+++ src/main/java/com/kitapmobile/Spring/Project/For/Mobile/Book/App/entity/Category.java
@@ -1,4 +1,18 @@
 package com.kitapmobile.Spring.Project.For.Mobile.Book.App.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
 public class Category {
+
+    @Column(name = "category_id")
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private  Long categoryId;
+
+    @Column(name = "category_name")
+    private String categoryName;
+
 }
