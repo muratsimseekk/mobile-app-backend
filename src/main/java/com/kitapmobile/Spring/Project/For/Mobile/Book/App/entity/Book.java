e6aec03f4bb17c96e1556f83f0eeee44d09ecdc8
Mgokayay <aymustafagokay@gmail.com>
2024-05-15 19:02:25 +0300

    two entities created

---
+++ src/main/java/com/kitapmobile/Spring/Project/For/Mobile/Book/App/entity/Book.java
@@ -0,0 +1,25 @@
+package com.kitapmobile.Spring.Project.For.Mobile.Book.App.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name = "book",schema = "mobile-app")
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+
+    @Column(name = "book_name")
+    private String bookName;
+
+    @Column(name = "estimated_time")
+    private String estimatedTime;
+}
