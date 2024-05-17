7c96e8e5e25a20c47ad394f94ea41e015e69a641
Murat Simsek <muratsimseekk@gmail.com>
2024-05-17 13:00:02 +0300

    Adds bookService and its implemention

---
+++ src/main/java/com/kitapmobile/Spring/Project/For/Mobile/Book/App/service/BookService.java
@@ -0,0 +1,20 @@
+package com.kitapmobile.Spring.Project.For.Mobile.Book.App.service;
+
+import com.kitapmobile.Spring.Project.For.Mobile.Book.App.dto.BookResponse;
+import com.kitapmobile.Spring.Project.For.Mobile.Book.App.entity.Book;
+
+import java.util.List;
+import java.util.Set;
+
+public interface BookService {
+
+    List<BookResponse> findAll();
+
+    BookResponse findById(Long id);
+
+    BookResponse save(Book book , List<Long> categoryIds);
+
+    Book findByBookId(Long id);
+
+    String delete(Long id);
+}
---
+++ src/main/java/com/kitapmobile/Spring/Project/For/Mobile/Book/App/service/BookServiceImpl.java
@@ -0,0 +1,68 @@
+package com.kitapmobile.Spring.Project.For.Mobile.Book.App.service;
+
+import com.kitapmobile.Spring.Project.For.Mobile.Book.App.dto.BookResponse;
+import com.kitapmobile.Spring.Project.For.Mobile.Book.App.entity.Book;
+import com.kitapmobile.Spring.Project.For.Mobile.Book.App.entity.Category;
+import com.kitapmobile.Spring.Project.For.Mobile.Book.App.factory.BookDtoConvertion;
+import com.kitapmobile.Spring.Project.For.Mobile.Book.App.repository.BookRepository;
+import com.kitapmobile.Spring.Project.For.Mobile.Book.App.repository.CategoryRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+
+@Service
+@AllArgsConstructor
+public class BookServiceImpl implements BookService{
+
+    private BookRepository bookRepository;
+    private CategoryRepository categoryRepository;
+    @Override
+    public List<BookResponse> findAll() {
+        return BookDtoConvertion.convertBookList(bookRepository.findAll());
+    }
+
+    @Override
+    public BookResponse findById(Long id) {
+        Optional<Book> optional= bookRepository.findById(id);
+        if(optional.isPresent()){
+            return BookDtoConvertion.convertBook(optional.get());
+        }
+        throw new RuntimeException("Girilen id de kitap bulunamadi. ID: " + id);
+    }
+
+    @Override
+    public BookResponse save(Book book , List<Long> categoryIds) {
+        //Bir Book'un birden fazla kategorisi olabilir .
+        // Bu yuzden birden fazla categoryId alacak bir list yolluyoruz ve bunlari categories altinda topluyoruz
+        //daha sonra Book icerisine hepsini ekliyoruz .
+        List<Category> categories = categoryRepository.findAllById(categoryIds);
+        book.getCategories().addAll(categories);
+
+//        for (Category category : categories){
+//            category.getBooks().add(book);
+//        }
+//
+         bookRepository.save(book);
+         return BookDtoConvertion.convertBook(book);
+    }
+
+    @Override
+    public Book findByBookId(Long id) {
+        Optional<Book> optional = bookRepository.findById(id);
+
+        if (optional.isPresent()){
+            return optional.get();
+        }
+        throw new RuntimeException("Girilen id de kitap bulunamadi. ID: " + id);
+    }
+
+    @Override
+    public String delete(Long id) {
+        Book book = findByBookId(id);
+         bookRepository.delete(book);
+        return "Kitap silme islemi basarili.";
+    }
+}
