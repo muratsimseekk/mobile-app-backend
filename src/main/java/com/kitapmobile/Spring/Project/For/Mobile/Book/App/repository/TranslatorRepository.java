1c04b9cba4892bfa77405b928d8a9ac4e539fbe1
Murat Simsek <muratsimseekk@gmail.com>
2024-05-30 00:31:27 +0300

    Creates TranslatorRepository

---
+++ src/main/java/com/kitapmobile/Spring/Project/For/Mobile/Book/App/repository/TranslatorRepository.java
@@ -0,0 +1,7 @@
+package com.kitapmobile.Spring.Project.For.Mobile.Book.App.repository;
+
+import com.kitapmobile.Spring.Project.For.Mobile.Book.App.entity.Translator;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TranslatorRepository extends JpaRepository<Translator , Long> {
+}
